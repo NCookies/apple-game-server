80d583045c702173bf81bf1840c6c4a6c5e3bebf
SeungWoo Ryu <swstar21c@gmail.com>
2025-02-01 17:29:05 +0900

    first commit

---
+++ src/main/java/xyz/ncookie/applegame/AppleGameServerApplication.java
@@ -0,0 +1,13 @@
+package xyz.ncookie.applegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppleGameServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppleGameServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/ncookie/applegame/AppleGameServerApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.ncookie.applegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleGameServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
