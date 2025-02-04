468ab6616dec99c6cac6531c2433a7a9a83ab2e8
SeungWoo Ryu <swstar21c@gmail.com>
2025-02-04 22:03:24 +0900

    #3 - csrf 비활성화
    - 해당 서비스는 REST API를 사용하기 때문에 csrf 옵션은 비활성화한다.

--- src/main/java/xyz/ncookie/applegame/config/SecurityConfig.java
+++ src/main/java/xyz/ncookie/applegame/config/SecurityConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -19,6 +19,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll())
                 .build();
